fec69cddfa5ac933c20250431d3e084c7cf75e67
l2k <ekyi0421@gmail.com>
2025-02-26 22:00:03 +0900

    feat: 그림 그리는 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import { useState } from 'react';
 import './App.scss';
 
 import "./styles/ColorThemes.scss"
+import DrawingCanvas from './components/DrawingCanvas';
+
+import ImageBackBlack from "./assets/images/image_back_black.png"
+import ImageBackLight from "./assets/images/image_back_light.png"
+import { ReactComponent as IconRefresh } from "./assets/images/icon_refresh.svg"
 
 function App() {
 
   const colorThemeList = ["light-theme", "dark-theme"];
   const [colorTheme, setColorTheme] = useState(colorThemeList[1]);
+
+  const [enabled, setEnabled] = useState(true);
+  const [isClear, setClear] = useState(false);
+
+  const onClear = () => {
+    setClear(true);
+    setEnabled(true);
+  };
 
   return (
     <div className={`App ${colorTheme}`} >
@@ -21,8 +34,18 @@ function App() {
           </div>
 
           <div className='title'>내 등을 부탁해!</div>
-          <div className='explain'>{`긁고 싶은 영역을 선택한 뒤 친구에게 요청해보세요 :)`}</div>
+          <div className='explain'>{`긁고 싶은 영역을 선택한 뒤 친구에게 보여주세요 ٩( ᐛ )و`}</div>
 
+          <button className='clear-button' onClick={() => onClear()}>
+            <IconRefresh className='img' />
+            {"초기화"}
+          </button>
+
+          <DrawingCanvas imageSrc={ImageBackLight} enabled={enabled} isClear={isClear} setClear={setClear} />
+
+          <button className='bottom-button' onClick={() => setEnabled((prev) => !prev)}>
+            {enabled ? "완료" : "다시하기"}
+          </button>
 
         </div>
       </div>
@@ -37,7 +60,6 @@ const ColorThemeBtn = ({ colorTheme, selected, onClick }) => {
     <button
       onClick={() => onClick(colorTheme)}
       className={`${colorTheme} color-theme-btn ${selected === colorTheme ? "selected" : ""}`}>
-
     </button>
   )
 }
