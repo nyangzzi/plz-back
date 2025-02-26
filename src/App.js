b24bc1016aa25206cc465feec8b6c7eb2dbd42a3
lek <ekyi0421@gmail.com>
2025-02-26 16:46:07 +0900

    feat: add color and font themes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
+
+import "./styles/ColorThemes.scss"
 
 function App() {
+
+  const colorThemeList = ["light-theme", "dark-theme"];
+  const [colorTheme, setColorTheme] = useState(colorThemeList[1]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${colorTheme}`} >
+
+      <div className='content-container'>
+        <div className='content'>
+
+          <div className='color-btn-list'>
+            {colorThemeList.map((item) =>
+              <ColorThemeBtn key={item} colorTheme={item} selected={colorTheme} onClick={setColorTheme} />
+            )}
+          </div>
+
+          <div className='title'>내 등을 부탁해!</div>
+          <div className='explain'>{`긁고 싶은 영역을 선택한 뒤 친구에게 요청해보세요 :)`}</div>
+
+
+        </div>
+      </div>
+
+
     </div>
   );
+}
+
+const ColorThemeBtn = ({ colorTheme, selected, onClick }) => {
+  return (
+    <button
+      onClick={() => onClick(colorTheme)}
+      className={`${colorTheme} color-theme-btn ${selected === colorTheme ? "selected" : ""}`}>
+
+    </button>
+  )
 }
 
 export default App;
